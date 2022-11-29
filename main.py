fbacc563696f12d277b9940ba984446ec1de1510
sean-wilkinson <42065483+sean-wilkinson@users.noreply.github.com>
2022-11-29 20:01:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import bme280
-from machine import Pin, I2C, SoftI2C
+from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import utime
 
-i2c0 = SoftI2C(scl=Pin(17), sda=Pin(16), freq=400000)
-i2c1 = I2C(1, scl=Pin(19), sda=Pin(18), freq=400000)
+i2c0 = I2C(0, scl=Pin(17), sda=Pin(16), freq=100000)
+i2c1 = I2C(1, scl=Pin(19), sda=Pin(18), freq=100000)
 
 oled = SSD1306_I2C(128, 32, i2c0)
 bme = bme280.BME280(i2c=i2c1)
@@ -20,5 +20,4 @@ while True:
     oled.show()
     
     utime.sleep(1)
-    
 
